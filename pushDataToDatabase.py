14d5632ac93bf6c1d67efa45f5149711e0687794
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-12-18 20:48:52 +0100

    Clear

--- pushDataToDatabase.py
+++ pushDataToDatabase.py
@@ -1,26 +1,21 @@
 import json
 import pandas as pd
 from pymongo import MongoClient
-# from pyspark import SQLContext
-# from pyspark.shell import sc
 
 client = MongoClient()
 db = client['Playback']
 songs_collection = db['songs']
+artists_collection = db['artists']
 
-
-# myrdd = sc.textFile("./data/unique_tracks.txt").map(lambda x: x.split('<SEP>'))
-
-
-# rawData = spark.read.csv("./data/unique_tracks.txt")
-# rawData = spark.read.format("csv").load("./data/unique_tracks.txt")
-# rawData.map(lambda x: x.split('<SEP>'))
-# print(myrdd.collect())
-
-colnames = ['variation', 'songId', 'title', 'artist']
+colnames = ['variation', 'songId', 'artist', 'title']
 df = pd.read_csv('./data/unique_tracks.txt', engine='python', sep='<SEP>', names=colnames, header=None)
 df = df.drop(columns=['variation'])
 df = df.drop_duplicates(subset=['songId'])
-
 records = json.loads(df.T.to_json()).values()
 songs_collection.insert(records)
+
+df_artists = df['artist']
+df_artists = df_artists.drop_duplicates()
+# records = json.loads(df_artists.T.to_json()).values()
+# artists_collection.insert(records)
+print(df_artists)
