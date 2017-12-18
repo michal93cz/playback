ff38c710b4bacbc28902017718ee7d99fc708e96
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-12-18 22:55:11 +0100

    Date and user collections

--- pushDataToDatabase.py
+++ pushDataToDatabase.py
@@ -1,26 +1,62 @@
 import json
 import pandas as pd
 from pymongo import MongoClient
+from datetime import datetime
 
 client = MongoClient()
 db = client['Playback']
+
+db.drop_collection('songs')
 songs_collection = db['songs']
+
+db.drop_collection('artists')
 artists_collection = db['artists']
+
+db.drop_collection('dates')
+dates_collection = db['dates']
+
+db.drop_collection('playbacks')
+playbacks_collection = db['playbacks']
+
+db.drop_collection('users')
+users_collection = db['users']
 
 colnames = ['variation', '_id', 'artist', 'title']
 df_songs = pd.read_csv('./data/unique_tracks.txt', engine='python', sep='<SEP>', names=colnames, header=None)
 df_songs = df_songs.drop(columns=['variation'])
 df_songs = df_songs.drop_duplicates(subset=['_id'])
 
+# ARTISTS
 df_artists = df_songs['artist']
 df_artists = df_artists.drop_duplicates()
 records = json.loads(df_artists.T.to_json()).values()
 records = list(map((lambda x: {'name': x}), records))
 artists_collection.insert(records)
 
+# SONGS
 records = json.loads(df_songs.T.to_json()).values()
 songs_collection.insert(records)
 
-colnames = ['userId', 'songId', 'time']
+# PLAYBACKS
+colnames = ['userId', 'songId', 'dateId']
 df_playback = pd.read_csv('./data/triplets_sample.txt', engine='python', sep='<SEP>', names=colnames, header=None)
-print(df_playback)
+
+# DATES
+df_dates = df_playback['dateId']
+df_dates = df_dates.drop_duplicates()
+records = json.loads(df_dates.T.to_json()).values()
+records = list(map((lambda x: {
+    '_id': x,
+    'day': datetime.fromtimestamp(x).day,
+    'month': datetime.fromtimestamp(x).month,
+    'year': datetime.fromtimestamp(x).year,
+    'weekday': datetime.fromtimestamp(x).weekday()
+}), records))
+dates_collection.insert(records)
+
+# USERS
+df_users = df_playback['userId']
+df_users = df_users.drop_duplicates()
+records = json.loads(df_dates.T.to_json()).values()
+records = list(map((lambda x: {'_id': x}), records))
+users_collection.insert(records)
