65a81032f2c5db277adaa7175ef67af61c334e94
damiana <d_antczak@op.pl>
2018-01-02 22:31:37 +0100

    playbacks by month query

--- queries.py
+++ queries.py
@@ -11,24 +11,24 @@ dates_collection = db['dates']
 playbacks_collection = db['playbacks']
 users_collection = db['users']
 
-# ranking piosenek
-start = time.time()
-pprint(list(playbacks_collection.aggregate([
-    {'$group': {'_id': '$songId', 'count': {'$sum': 1}}},
-    {'$sort': {'count': -1}},
-    {'$limit': 15},
-    {
-        '$lookup': {
-            'from': 'songs',
-            'localField': '_id',
-            'foreignField': '_id',
-            'as': 'song'
-        }
-    },
-    {'$match': {'song': {'$ne': []}}}
-])))
-end = time.time()
-print(end - start)
+# # ranking piosenek
+# start = time.time()
+# pprint(list(playbacks_collection.aggregate([
+#     {'$group': {'_id': '$songId', 'count': {'$sum': 1}}},
+#     {'$sort': {'count': -1}},
+#     {'$limit': 15},
+#     {
+#         '$lookup': {
+#             'from': 'songs',
+#             'localField': '_id',
+#             'foreignField': '_id',
+#             'as': 'song'
+#         }
+#     },
+#     {'$match': {'song': {'$ne': []}}}
+# ])))
+# end = time.time()
+# print(end - start)
 
 # ranking użytkowników
 # start = time.time()
@@ -58,3 +58,32 @@ print(end - start)
 # ])))
 # end = time.time()
 # print(end - start)
+
+
+# start = time.time()
+# pprint(list(playbacks_collection.aggregate([
+#     {'$group': {'_id': '$songId', 'count': {'$sum': 1}}},
+#     {'$sort': {'count': -1}},
+#     {'$limit': 15},
+#     {
+#         '$lookup': {
+#             'from': 'songs',
+#             'localField': '_id',
+#             'foreignField': '_id',
+#             'as': 'song'
+#         }
+#     },
+#     {'$match': {'song': {'$ne': []}}}
+# ])))
+# end = time.time()
+# print(end - start)
+
+
+# sumaryczna liczba odsłuchań w podziale na poszczególne miesiące
+start = time.time()
+pprint(list(dates_collection.aggregate([
+    {'$group': {'_id': '$month', 'count': {'$sum': 1}}},
+    {'$sort': {'_id': 1}}
+])))
+end = time.time()
+print(end - start)
