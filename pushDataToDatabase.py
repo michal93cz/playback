e32b4ce3f4600f728d8b3c725225820fc5ccc0c5
Czupryniak, Michal <michal.czupryniak@pearson.com>
2018-01-02 21:49:06 +0100

    Update

--- pushDataToDatabase.py
+++ pushDataToDatabase.py
@@ -3,6 +3,7 @@ from bson.json_util import loads
 import pandas as pd
 from pymongo import MongoClient
 from datetime import datetime
+from pprint import pprint
 
 client = MongoClient()
 db = client['Playback']
@@ -32,29 +33,37 @@ df_songs = df_songs.drop_duplicates(subset=['_id'])
 # ARTISTS
 df_artists = df_songs['artist']
 df_artists = df_artists.drop_duplicates()
-records = json.loads(df_artists.T.to_json()).values()
-records = list(map((lambda x: {'name': x}), records))
+records = json.loads(df_artists.T.to_json()).items()
+records = list(map((lambda x: {'_id': x[0], 'name': x[1]}), records))
 artists_collection.insert(records)
 
+
 # PLAYBACKS
+print('playback read start')
 colnames = ['userId', 'songId', 'dateId']
-df_playback = pd.read_csv('./data/triplets_sample_20p.txt', engine='python', sep='<SEP>', names=colnames, header=None,
-                          nrows=50000)
-
+df_playback = pd.read_csv('./data/triplets_sample_20p.txt', engine='python', sep='<SEP>', names=colnames, header=None
+                          , nrows=10000)
+print('playback list start')
 records = list(loads(df_playback.T.to_json()).values())
-for index, row in df_playback.iterrows():
-    print(index)
-    song_row = df_songs.loc[df_songs['_id'] == row['songId']]
-    artist_name = song_row.iloc[0]['artist']
-    artist_id = artists_collection.find_one({'name': artist_name})['_id']
-    records[index]['artistId'] = artist_id
 
+# TO MUSIMY ZASTĄPIC:
+# for index, row in df_playback.iterrows():
+#     print(index)
+#     song_row = df_songs.loc[df_songs['_id'] == row['songId']]
+#     artist_name = song_row.iloc[0]['artist']
+#     artist_id = artists_collection.find_one({'name': artist_name})['_id']
+#     records[index]['artistId'] = artist_id
+
+print('playback insert start')
 playbacks_collection.insert(records)
+print('playback inserted')
+
 
 # SONGS
 df_songs.drop('artist', axis=1, inplace=True)
 records = json.loads(df_songs.T.to_json()).values()
 songs_collection.insert(records)
+
 
 # DATES
 df_dates = df_playback['dateId']
@@ -68,6 +77,7 @@ records = list(map((lambda x: {
     'weekday': datetime.fromtimestamp(x).weekday()
 }), records))
 dates_collection.insert(records)
+
 
 # USERS
 df_users = df_playback['userId']
--- queries.py
+++ queries.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import time
 
 from pymongo import MongoClient
 
@@ -11,40 +12,49 @@ playbacks_collection = db['playbacks']
 users_collection = db['users']
 
 # ranking piosenek
-# pprint(list(playbacks_collection.aggregate([
-#     {'$group': {'_id': '$songId', 'count': {'$sum': 1}}},
-#     {'$sort': {'count': -1}},
-#     {'$limit': 15},
-#     {
-#         '$lookup': {
-#             'from': 'songs',
-#             'localField': '_id',
-#             'foreignField': '_id',
-#             'as': 'song'
-#         }
-#     },
-#     {'$match': {'song': {'$ne': []}}}
-# ])))
+start = time.time()
+pprint(list(playbacks_collection.aggregate([
+    {'$group': {'_id': '$songId', 'count': {'$sum': 1}}},
+    {'$sort': {'count': -1}},
+    {'$limit': 15},
+    {
+        '$lookup': {
+            'from': 'songs',
+            'localField': '_id',
+            'foreignField': '_id',
+            'as': 'song'
+        }
+    },
+    {'$match': {'song': {'$ne': []}}}
+])))
+end = time.time()
+print(end - start)
 
 # ranking użytkowników
+# start = time.time()
 # pprint(list(playbacks_collection.aggregate([
 #     {'$group': {'_id': '$userId', 'count': {'$sum': 1}}},
 #     {'$sort': {'count': -1}},
 #     {'$limit': 15}
 # ])))
+# end = time.time()
+# print(end - start)
 
 # ranking artystów
-pprint(list(playbacks_collection.aggregate([
-    {'$group': {'_id': '$artistId', 'count': {'$sum': 1}}},
-    {'$sort': {'count': -1}},
-    {'$limit': 15},
-    {
-        '$lookup': {
-            'from': 'artists',
-            'localField': '_id',
-            'foreignField': '_id',
-            'as': 'artist'
-        }
-    },
-    {'$match': {'artist': {'$ne': []}}}
-])))
+# start = time.time()
+# pprint(list(playbacks_collection.aggregate([
+#     {'$group': {'_id': '$artistId', 'count': {'$sum': 1}}},
+#     {'$sort': {'count': -1}},
+#     {'$limit': 15},
+#     {
+#         '$lookup': {
+#             'from': 'artists',
+#             'localField': '_id',
+#             'foreignField': '_id',
+#             'as': 'artist'
+#         }
+#     },
+#     {'$match': {'artist': {'$ne': []}}}
+# ])))
+# end = time.time()
+# print(end - start)
